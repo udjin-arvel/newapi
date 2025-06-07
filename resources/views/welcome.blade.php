8833cab7a285f1288f273b6508f2ad61990db727
arvelov <udjin.arvel@gmail.com>
2025-06-07 21:19:46 +0700

    add meta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,18 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta property="og:title" content="Arvelov">
+        <meta property="og:description" content="Частная web-студия">
+        <meta property="og:image" content="https://arvelov.online/logo.png">
+        <meta property="og:url" content="https://arvelov.online">
+        <meta property="og:type" content="website">
+
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="Arvelov">
+        <meta name="twitter:description" content="Частная web-студия">
+        <meta name="twitter:image" content="https://arvelov.online/logo.png">
+
         <title>Arvelov</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
