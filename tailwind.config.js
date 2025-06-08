7f561b8e7284340b6d1f6907939368ac669307c7
arvelov <udjin.arvel@gmail.com>
2025-06-08 22:43:51 +0700

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.css",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.css",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    safelist: [
+        'bg-[url(/img/development.jpg)]',
+        'bg-[url(/img/telegram.jpg)]',
+        'bg-[url(/img/application.jpg)]',
+        'bg-[url(/img/lk.jpg)]',
+        'bg-[url(/img/tech.jpg)]',
+        'bg-[url(/img/consulting.jpg)]',
+    ],
 }
 
